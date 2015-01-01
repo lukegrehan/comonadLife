19adc7b94c0c8712091ef6e85cbfe7490e1f005f
Luke Grehan <luketenant@gmail.com>
2015-01-01 18:43:43 +0000

    cleanup

--- comonadLife.hs
+++ comonadLife.hs
@@ -18,10 +18,10 @@ extractL (Linear _ a _) = a
 
 toListL :: Int -> Linear a -> [a]
 toListL size (Linear ls c rs) =
-	(reverse $ take size ls) ++ [c] ++ take size rs
+  (reverse $ take size ls) ++ [c] ++ take size rs
 
 instance Functor Linear where
-	fmap  f (Linear ls c rs) = Linear (map f ls) (f c) (map f rs)
+  fmap  f (Linear ls c rs) = Linear (map f ls) (f c) (map f rs)
 
 {----------------------------------------------------------------------------}
 
@@ -44,24 +44,24 @@ extractP :: Planar a -> a
 extractP (Planar lines) = extractL $ extractL lines
 
 instance Functor Planar where
-	fmap f (Planar lines) = Planar (fmap (fmap f) lines)
+  fmap f (Planar lines) = Planar (fmap (fmap f) lines)
 
 instance Show a => Show (Planar a) where
-	-- not strictly correct since the plane is infinite...
-	--   but thats not very useful so show a subset of it
-	show (Planar lines) = unlines $ map showLine $ toListL 5 lines
-		where
-			showLine l = concatMap show $ toListL 5 l
+  -- not strictly correct since the plane is infinite...
+  --   but thats not very useful so show a subset of it
+  show (Planar lines) = unlines $ map showLine $ toListL 5 lines
+    where
+      showLine l = concatMap show $ toListL 5 l
 
 {----------------------------------------------------------------------------}
 
 move :: (f a -> f a) -> (f a -> f a)
-	        -> f a -> Linear (f a)
+          -> f a -> Linear (f a)
 move l r z = Linear ls z rs
-	where
-		ls = iterate' l z
-		rs = iterate' r z
-		iterate' f = tail . iterate f
+  where
+    ls = iterate' l z
+    rs = iterate' r z
+    iterate' f = tail . iterate f
 
 fromListWithDefault :: a -> [[a]] -> Planar a
 fromListWithDefault def ls =
@@ -70,13 +70,12 @@ fromListWithDefault def ls =
       rs = (map mkLine ls) ++ repeat deadLine
       deadLine = Linear (repeat def) def (repeat def)
       mkLine l = Linear (repeat def) def (l ++ (repeat def))
---fromListWithDefault _ _ = error ""
 
 instance Comonad Planar where
-	extract = extractP
-	duplicate p = Planar
-		$   (move left right)
-		<$> (move up down p)
+  extract = extractP
+  duplicate p = Planar
+    $   (move left right)
+    <$> (move up down p)
 
 {----------------------------------------------------------------------------}
 
@@ -88,10 +87,10 @@ type Rule2d a = (Planar a -> a)
 --  center cell for convenience
 neighbours :: Planar a -> [a]
 neighbours p =
-	map extract
-	$ map ($ p)
-	$	[(.)] `ap` [up,id,down] `ap` [left, id, right]
-	-- sometimes haskell makes me feel like a wizard...
+  map extract
+  $ map ($ p)
+  $ [(.)] `ap` [up,id,down] `ap` [left, id, right]
+  -- sometimes haskell makes me feel like a wizard...
 
 -- Allow a rule that operates on some list of concrete neighbours to be
 --  'lifted' into the 2d automata
@@ -100,11 +99,11 @@ liftRule2d rule = rule.neighbours
 
 data Cell = Alive | Dead deriving (Eq)
 instance Show Cell where
-	show Alive = "*"
-	show Dead  = " "
+  show Alive = "*"
+  show Dead  = " "
 instance Read Cell where
-	readsPrec _ "*" = [(Alive,"")]
-	readsPrec _ " " = [(Dead,"")]
+  readsPrec _ "*" = [(Alive,"")]
+  readsPrec _ " " = [(Dead,"")]
 
 fromList :: [[Cell]] -> Planar Cell
 fromList = fromListWithDefault Dead
@@ -115,24 +114,24 @@ type Board = Planar Cell
 
 ruleGol :: Rule2d Cell
 ruleGol = liftRule2d gol
-	where
-		gol [
-			a, b, c,
-			d, x, f,
-			g, h, i] =
-				case aliveNeighours of
-					2 -> x
-					3 -> Alive
-					_ -> Dead
-			where
-				aliveNeighours = (length $ filter (==Alive) $ [a,b,c,d,f,g,h,i])
-		gol _ = error "gol applied to invalid board"
+  where
+    gol [
+      a, b, c,
+      d, x, f,
+      g, h, i] =
+        case aliveNeighours of
+          2 -> x
+          3 -> Alive
+          _ -> Dead
+      where
+        aliveNeighours = (length $ filter (==Alive) $ [a,b,c,d,f,g,h,i])
+    gol _ = error "gol applied to invalid board"
 
 glider :: Board
 glider = fromList $ map (map read) [
-										[ " ", "*", " " ],
-										[ " ", " ", "*" ],
-										[ "*", "*", "*" ]]
+                    [ " ", "*", " " ],
+                    [ " ", " ", "*" ],
+                    [ "*", "*", "*" ]]
 
 blinker :: Board
 blinker = fromList $ map (map read) [[ "*", "*", "*" ]]
@@ -141,10 +140,10 @@ blinker = fromList $ map (map read) [[ "*", "*", "*" ]]
 plane =>>> rule = iterate (=>> rule) plane
 
 main = do
-	forM_ (take 10 (glider =>>> ruleGol)) $ do
-		print
+  forM_ (take 10 (glider =>>> ruleGol)) $ do
+    print
 
-	putStrLn "-----"
+  putStrLn "-----"
 
-	forM_ (take 10 (blinker =>>> ruleGol)) $ do
-		print
+  forM_ (take 10 (blinker =>>> ruleGol)) $ do
+    print
